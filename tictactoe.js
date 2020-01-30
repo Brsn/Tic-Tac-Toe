ea19a3fe81f7d1fddd402b8238b6d55afbf4c140
Bronson Brown <Brsn90@icloud.com>
2020-01-29 19:53:19 -0700

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -6,7 +6,7 @@
 let playerTurn = 'X';
 let previousTurn = 'O';
 let aiPick = 0;
-
+let squaresFilled = 0;
 
 
 let squareX = {
@@ -16,6 +16,10 @@ let squareY = {
     1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '',
 }
 
+let squaresXMoves = [];
+
+
+
 function aiMove() {
     let turn = playerTurn;
     while (playerTurn != 'X')
@@ -24,6 +28,8 @@ function aiMove() {
             document.getElementById(aiPick).innerHTML = turn;
             squareY[aiPick] = 'O';
             previousTurn = 'O';
+            squaresFilled++;
+            console.log(squaresFilled);
             playerTurn = 'X';
             checkForWin();
         }
@@ -42,38 +48,15 @@ function checkForWin() {
     /*Sixteen possible ways to win. Also Holy if statements. We have two object literals that store 1-9 x or o values. 
     Once three in a row feel in one of those objects we have a winner. Also page will refresh upon winning.*/
 
-    if ((squareX[1] === 'X' && squareX[2] === 'X' && squareX[3] === 'X') || (squareY[1] === 'O' && squareY[2] === 'O' && squareY[3] === 'O')) {
-        alert(previousTurn + " is the Winner!");
-        window.location.reload();
-    }
-    if ((squareX[4] === 'X' && squareX[5] === 'X' && squareX[6] === 'X') || (squareY[4] === 'O' && squareY[5] === 'O' && squareY[6] === 'O')) {
-        alert(previousTurn + " is the Winner!");
-        window.location.reload();
-    }
-    if ((squareX[1] === 'X' && squareX[4] === 'X' && squareX[7] === 'X') || (squareY[1] === 'O' && squareY[4] === 'O' && squareY[7] === 'O')) {
-        alert(previousTurn + " is the Winner!");
-        window.location.reload();
-    }
-    if ((squareX[2] === 'X' && squareX[5] === 'X' && squareX[8] === 'X') || (squareY[2] === 'O' && squareY[5] === 'O' && squareY[8] === 'O')) {
-        alert(previousTurn + " is the Winner!");
-        window.location.reload();
-    }
-    if ((squareX[3] === 'X' && squareX[6] === 'X' && squareX[9] === 'X') || (squareY[3] === 'O' && squareY[6] === 'O' && squareY[9] === 'O')) {
-        alert(previousTurn + " is the Winner");
-        window.location.reload();
-    }
-    if ((squareX[1] === 'X' && squareX[5] === 'X' && squareX[9] === 'X') || (squareY[1] === 'O' && squareY[5] === 'O' && squareY[9] === 'O')) {
-        alert(previousTurn + " is the Winner");
-        window.location.reload();
-    }
-    if ((squareX[3] === 'X' && squareX[5] === 'X' && squareX[9] === 'X') || (squareY[3] === 'O' && squareY[5] === 'O' && squareY[9] === 'O')) {
-        alert(previousTurn + " is the Winner");
-        window.location.reload();
-    }
-    if ((squareX[7] === 'X' && squareX[8] === 'X' && squareX[9] === 'X') || (squareY[7] === 'O' && squareY[8] === 'O' && squareY[9] === 'O')) {
-        alert(previousTurn + " is the Winner");
-        window.location.reload();
-    }
+    if ((squareX[1] === 'X' && squareX[2] === 'X' && squareX[3] === 'X') || (squareY[1] === 'O' && squareY[2] === 'O' && squareY[3] === 'O')) { }
+    if ((squareX[4] === 'X' && squareX[5] === 'X' && squareX[6] === 'X') || (squareY[4] === 'O' && squareY[5] === 'O' && squareY[6] === 'O')) { }
+    if ((squareX[1] === 'X' && squareX[4] === 'X' && squareX[7] === 'X') || (squareY[1] === 'O' && squareY[4] === 'O' && squareY[7] === 'O')) { }
+    if ((squareX[2] === 'X' && squareX[5] === 'X' && squareX[8] === 'X') || (squareY[2] === 'O' && squareY[5] === 'O' && squareY[8] === 'O')) { }
+    if ((squareX[3] === 'X' && squareX[6] === 'X' && squareX[9] === 'X') || (squareY[3] === 'O' && squareY[6] === 'O' && squareY[9] === 'O')) { }
+    if ((squareX[1] === 'X' && squareX[5] === 'X' && squareX[9] === 'X') || (squareY[1] === 'O' && squareY[5] === 'O' && squareY[9] === 'O')) { }
+    if ((squareX[3] === 'X' && squareX[5] === 'X' && squareX[9] === 'X') || (squareY[3] === 'O' && squareY[5] === 'O' && squareY[9] === 'O')) { }
+    if ((squareX[7] === 'X' && squareX[8] === 'X' && squareX[9] === 'X') || (squareY[7] === 'O' && squareY[8] === 'O' && squareY[9] === 'O')) { }
+
 }
 
 function delayAi() {
@@ -92,6 +75,10 @@ function setSelection(val) {
         if (document.getElementById(value).innerHTML === 'Click') {
             document.getElementById(value).innerHTML = turn;
             previousTurn = 'X'
+            squaresXMoves.push(value);
+            squaresFilled++;
+            console.log(squaresFilled);
+            console.log(squaresXMoves);
             playerTurn = 'O'
             checkForWin();
         }
