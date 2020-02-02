5207b92083aa5cfde2c00ccacd4d45ab57d5223a
Bronson Brown <Brsn90@icloud.com>
2020-02-02 09:26:21 -0700

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -14,11 +14,28 @@ let gameOver = false;
 var won = false;
 let bestYMove = 0;
 let bestXMove = 0;
+var oWon = localStorage.getItem("O")
+var xWon = localStorage.getItem("X")
+var draw = localStorage.getItem("draw")
 
-//set the local storage to a variable so we can increment also needed to parse strings.
-let xWon = parseInt(localStorage.getItem("X"));
-let oWon = parseInt(localStorage.getItem("O"));
-let draw = parseInt(localStorage.getItem("draw"));
+
+/*Function parseValues, will prevent NaN from persisting in local storage.
+Can't increment NaN so score won't be kept */
+function parseValues() {
+    if (localStorage.getItem("O") !== null && localStorage.getItem("X") !== null && localStorage.getItem("draw" !== null)) {
+
+        oWon = parseInt(localStorage.getItem("O"));
+        xWon = parseInt(localStorage.getItem("X"));
+        draw = parseInt(localStorage.getItem("draw"))
+    }
+}
+parseValues();
+
+//DOM Values manipulated with scores.
+document.getElementById("playerWins").innerHTML = localStorage.getItem("X");
+document.getElementById("computerWins").innerHTML = localStorage.getItem("O");
+document.getElementById("draw").innerHTML = localStorage.getItem("draw");
+
 
 let squareX = {
     1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '',
@@ -28,9 +45,7 @@ let squareY = {
 }
 
 
-document.getElementById("draw").innerHTML = localStorage.getItem("draw");
-document.getElementById("computerWins").innerHTML = localStorage.getItem("O");
-document.getElementById("playerWins").innerHTML = localStorage.getItem("X");
+
 
 console.log(localStorage);
 
@@ -40,7 +55,7 @@ function checkAheadX() {
     bestXMove = 0;
     //bestXMove will equal a game winning move
     for (let i = 1; i <= 9; i++) {
-        if (document.getElementById(i).innerHTML === 'Click') {
+        if (document.getElementById(i).innerHTML === '*') {
             squareX[i] = 'X';
             //Test to see if SquareX object literal has numerical value already
             if (checkForWin()) {
@@ -61,35 +76,35 @@ function checkAheadX() {
     console.log(squareY)
     console.log(squareX)
 }
-function checkAheadY(...squareY) {
-    let tempSquareY = squareY;
+function checkAheadY() {
 
     bestYMove = 0;
     let loop = 0;
-    generatePick();
     while (loop === 0) {
-        if (tempSquareY[aiPick] !== 'Click') {
-            generatePick();
+        for (let i = 1; i < 9; i++) {
+            if (document.getElementById(i).innerHTML === '*') {
+                let tempSquareY = { ...squareY };
+                console.log("The temp", tempSquareY);
+                tempSquareY[i] = 'O';
+                if (checkForWin()) {
+                    //Test to see if SquareY object literal has numerical value already
+                    bestYMove = i;
+                    //We found a best move, let's store it.
+                    //squareY[i] = '';
+                    //Here we reset squareY object literal
+                    gameOver = false;
+                    console.log("bestYMove", bestYMoves)
+                }
+                else {
+
+                    //  tempSquareY[aiPick] = '';
+                }
+            }
+            console.log("bestYMove", bestYMove)
         }
-        if (document.getElementById(aiPick).innerHTML === 'Click' && checkForWin()) {
-            tempSquareY[aiPick] = 'O';
-            //Test to see if SquareY object literal has numerical value already
-            bestYMove = aiYPick;
-            aiYPick = bestYMove;
-            //We found a best move, let's store it.
-            //squareY[i] = '';
-            //Here we reset squareY object literal
-            gameOver = false;
-        }
-        else {
-            loop++;
-            //  tempSquareY[aiPick] = '';
-        }
-        console.log(squareY)
-        console.log(squareX)
-        console.log("bestYMove", bestYMove)
+        loop++;
     }
-    console.log("bestYMove", bestYMove)
+
 }
 
 
@@ -116,7 +131,7 @@ function aiMove() {
         if (bestYMove > 0 && move === 0) {
             aiYPick = bestYMove;
             console.log(aiYPick, "From best move from Y");
-            if (document.getElementById(aiYPick).innerHTML === 'Click') {
+            if (document.getElementById(aiYPick).innerHTML === '*') {
                 document.getElementById(aiYPick).innerHTML = 'O';
                 squareY[aiYPick] = 'O';
                 squaresFilled++;
@@ -131,7 +146,7 @@ function aiMove() {
         if (bestXMove > 0 && move === 0) {
             aiXPick = bestXMove;
             console.log(aiXPick, "From best move from X");
-            if (document.getElementById(aiXPick).innerHTML === 'Click') {
+            if (document.getElementById(aiXPick).innerHTML === '*') {
                 document.getElementById(aiXPick).innerHTML = 'O';
                 squareY[aiXPick] = 'O';
                 squaresFilled++;
@@ -148,7 +163,7 @@ function aiMove() {
             generatePick()
             console.log(aiPick, "From generate the pick")
             //Ai has a random move until it's possible to win at 3+ turns
-            if (document.getElementById(aiPick).innerHTML === 'Click') {
+            if (document.getElementById(aiPick).innerHTML === '*') {
                 document.getElementById(aiPick).innerHTML = 'O';
                 squareY[aiPick] = 'O';
                 squaresFilled++;
@@ -236,9 +251,9 @@ function setSelection(val) {
     let turn = playerTurn;
     let value = val;
     if (turn === 'X') {
-        squareX[value] = 'X';
-        if (document.getElementById(value).innerHTML === 'Click') {
+        if (document.getElementById(value).innerHTML === '*') {
             document.getElementById(value).innerHTML = 'X';
+            squareX[value] = 'X';
             squaresFilled++;
             console.log(squaresFilled);
             playerTurn = 'O';
@@ -255,23 +270,32 @@ function setSelection(val) {
 function winner() {
     if (gameOver === true) {
         alert(prevTurn + " Is The Winner!")
-        if (turn = 'X') {
+        if (prevTurn === 'X') {
             xWon++;
+            parseValues();
             localStorage.setItem('X', xWon);
+            parseValues();
+
+            console.log(xWon);
             window.location.reload();
         }
-        if (turn = 'O') {
+        if (prevTurn === 'O') {
             oWon++;
             localStorage.setItem('O', oWon);
+            parseValues();
+            console.log(oWon);
             window.location.reload();
         }
-        else if (squaresFilled === 9) {
-            alert("Draw")
-            draw++;
-            localStorage.setItem('draw', draw);
-            window.location.reload();
+    }
+    else if (squaresFilled >= 9) {
+        alert("Draw!");
+        draw++;
+        parseValues();
+        localStorage.setItem('draw', draw);
 
-        }
+        console.log(draw);
+
+        window.location.reload()
     }
 }
 
